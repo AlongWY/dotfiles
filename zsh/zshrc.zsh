148aeeb54bd5c02d802a2fb530546a5720635903
Along <20281571+AlongWY@users.noreply.github.com>
2022-03-08 14:27:32 +0800

    Update zshrc.zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,135 +1,75 @@
-ZINIT="$HOME/.zinit"
-ZINIT_GIT="https://github.com.cnpmjs.org/zdharma/zinit.git"
+# INIT HOMEBREW
+eval "$(/usr/local/bin/brew shellenv)"
+# eval "$(sheldon source)"
 
-# Install zinit if not exist
-if [ ! -d "$ZINIT" ]; then
-    echo "Installing zinit ..."
+# INSTALL ZI
+if [ ! -d "$ZI_HOME" ]; then
+    echo "Installing zi ..."
     if [ -x "$(which git)" ]; then
-        mkdir -p "$ZINIT" 2> /dev/null
-        git clone "$ZINIT_GIT" "$ZINIT/bin"
+        mkdir -p "$ZI_HOME" 2> /dev/null
+        git clone "$ZI_GIT" "$ZI_HOME/bin"
     else
         echo "ERROR: please install git before installation !!"
     fi
     if [ ! $? -eq 0 ]; then
         echo ""
-        echo "ERROR: downloading zinit ($ZINIT_GIT) failed !!"
-        rm -rf $ZINIT
+        echo "ERROR: downloading zinit ($ZI_GIT) failed !!"
+        rm -rf $ZI_HOME
     fi;
 fi
 
-# INIT ZINIT
-source $ZINIT/bin/zinit.zsh
-
-# 平台判断 {{{
-if [[ "$(uname)" == "Darwin" ]]; then
-# Mac OS X 操作系统
-PICK="*darwin*"
-
-elif [[ "$(uname)" == "Linux" ]]; then
-# GNU/Linux操作系统
-PICK="*musl*"
-
-elif [[ "$(uname)" == "MINGW32_NT" ]]; then
-# Windows NT操作系统
-PICK="*windows*"
-
-fi
-# }}}
-
-# 安装插件 {{{
-
-# sharkdp/bat
-if [ ! -x "$(which bat)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"bat* -> bat" pick"bat/bat"
-    zinit light sharkdp/bat
-fi
-
-# sharkdp/fd
-if [ ! -x "$(which fd)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"fd* -> fd" pick"fd/fd"
-    zinit light sharkdp/fd
-fi
-
-# sharkdp/hyperfine
-if [ ! -x "$(which hyperfine)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"hyperfine* -> hyperfine" pick"hyperfine/hyperfine"
-    zinit light sharkdp/hyperfine
-fi
-
-# Peltoche/lsd
-if [ ! -x "$(which lsd)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"lsd* -> lsd" pick"lsd/lsd"
-    zinit light Peltoche/lsd
-fi
-
-# BurntSushi/ripgrep
-if [ ! -x "$(which rg)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"ripgrep* -> ripgrep" pick"ripgrep/rg"
-    zinit light BurntSushi/ripgrep
-fi
-
-# XAMPPRocky/tokei
-if [ ! -x "$(which tokei)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"tokei* -> tokei" pick"tokei/tokei"
-    zinit light XAMPPRocky/tokei
-fi
-
-# starship/starship
-if [ ! -x "$(which starship)" ]; then
-    zinit ice as"command" from"gh-r" bpick"$PICK" mv"starship* -> starship" pick"starship/starship"
-    zinit light starship/starship
-fi
-
-# Binary release in archive, from GitHub-releases page.
-# After automatic unpacking it provides program "fzf".
-if [ ! -x "$(which fzf)" ]; then
-    zinit ice from"gh-r" as"program"
-    zinit light junegunn/fzf-bin
-fi
-
-# }}}
-
-# ENV
+# INIT ZI
+source $ZI_HOME/bin/zi.zsh
 
 # 快速目录跳转
-zinit ice lucid wait='1'
-zinit light skywind3000/z.lua
+zi ice lucid wait='1'
+zi light skywind3000/z.lua
 
 # 语法高亮
-zinit ice lucid wait='0' atinit='zpcompinit'
-zinit light zdharma/fast-syntax-highlighting
+zi ice lucid wait='0' atinit='zpcompinit'
+zi light zdharma/fast-syntax-highlighting
 
 # 自动建议
-zinit ice lucid wait="0" atload='_zsh_autosuggest_start'
-zinit light zsh-users/zsh-autosuggestions
+zi ice lucid wait="0" atload='_zsh_autosuggest_start'
+zi light zsh-users/zsh-autosuggestions
 
 # 补全
-zinit ice lucid wait='0'
-zinit light zsh-users/zsh-completions
+zi ice lucid wait='0'
+zi light zsh-users/zsh-completions
 
 # 加载 OMZ 框架及部分插件
-zinit snippet OMZ::lib/completion.zsh
-zinit snippet OMZ::lib/history.zsh
-zinit snippet OMZ::lib/key-bindings.zsh
-zinit snippet OMZ::lib/theme-and-appearance.zsh
-zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
-zinit snippet OMZ::plugins/extract
+zi snippet OMZ::lib/completion.zsh
+zi snippet OMZ::lib/history.zsh
+zi snippet OMZ::lib/key-bindings.zsh
+zi snippet OMZ::lib/theme-and-appearance.zsh
+zi snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
+zi snippet OMZ::plugins/extract
 
-zinit ice lucid wait='1'
-zinit snippet OMZ::plugins/git/git.plugin.zsh
+zi ice lucid wait='1'
+zi snippet OMZ::plugins/git/git.plugin.zsh
+
+zi ice lucid wait as'completion' blockf has'cargo'
+zi snippet https://github.com/rust-lang/cargo/blob/master/src/etc/_cargo
 
 DISABLE_LS_COLORS=true
 
+# BAT 支持鼠标滚动
+export BAT_PAGER="less -RF"
+
 # alias
-alias ls='lsd'
+alias ls='exa --icons'
 alias l='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
-alias cat='bat' 
+# alias cat='bat' 
+alias cp='xcp'
 alias grep='rg' 
 alias find='fd' 
 alias cloc='tokei' 
+alias ps='procs' 
+alias sed='sd' 
+alias du='dust' 
 
 # 配置 fzf 使用 fd
 export FZF_DEFAULT_COMMAND='fd --type f'
@@ -146,3 +86,12 @@ if [ -x "$(which starship)" ]; then
     eval "$(starship init zsh)"
 fi
 
+# rbenv
+eval "$(rbenv init -)"
+
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+
+  autoload -Uz compinit
+  compinit
+fi
